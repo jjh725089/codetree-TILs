be2ddd42f9b8779b450649d92b28f2165367fb21
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-02-26 16:51:01 +0900

    Title: 좌우로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 2 == 0)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                cout << j + 1;
+            }
+        }
+        else
+        {
+            for(int j = 0; j < n; j++)
+            {
+                cout << n - j;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
