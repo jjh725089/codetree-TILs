57ed35ed867bccfaa971ba4eccca7a068bc64388
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-02-26 16:45:41 +0900

    Title: 9-1 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/9-1 정사각형/9-1-square.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt;
+    cin >> n;
+    cnt = 9;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(cnt == 0)
+            {
+                cnt = 9;
+            }
+            cout << cnt;
+            cnt--;
+        }
+        cout << endl;
+    }
+    return 0;
+}
