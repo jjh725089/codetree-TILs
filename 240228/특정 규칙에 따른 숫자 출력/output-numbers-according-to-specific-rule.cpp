c428b87960f9acab0c5420f5ae25cdad0c20b1b7
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-02-28 14:34:55 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240228/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.cpp
+++ 240228/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.cpp
@@ -7,8 +7,6 @@ int main() {
     cnt = 1;
     for(int i = 0; i < n; i++)
     {
-        if(cnt == 10)
-        cnt = 1;
         for(int j = 0; j < i; j++)
         {
             cout << "  ";
@@ -17,6 +15,8 @@ int main() {
         {
             cout << cnt << " ";
             cnt++;
+            if(cnt >= 10)
+            cnt = 1;
         }
         cout << endl;
     }
