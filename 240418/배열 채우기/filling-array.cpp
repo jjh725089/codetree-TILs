cbd52da54fb911529bb2271d8b24502726243783
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-04-18 23:07:45 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240418/배열 채우기/filling-array.cpp
+++ 240418/배열 채우기/filling-array.cpp
@@ -2,19 +2,30 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    int arr[10],j;
+    int arr[10],j,flag = 0;
     for(int i = 0; i < 10; i++)
     {
         cin >> arr[i];
         if(arr[i] == 0)
         {
             j = i;
+            flag = 1;
             break;
         }
     }
-    for(int i = 1; i <= j; i++)
+    if (flag == 1)
     {
-        cout << arr[j - i] << " ";
+        for(int i = 1; i <= j; i++)
+        {
+            cout << arr[j - i] << " ";
+        }
+    }
+    else
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            cout << arr[10 - i - 1] << " ";
+        }
     }
     return 0;
 }
