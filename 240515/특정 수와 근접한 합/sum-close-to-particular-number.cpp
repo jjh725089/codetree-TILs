8b9db4624f409fc422e1700f623134c416dde19e
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-05-15 20:40:44 +0900

    Title: 특정 수와 근접한 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240515/특정 수와 근접한 합/sum-close-to-particular-number.cpp
+++ 240515/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    int N, S, n_sum = 0, tn_sum, s_val, min, MIN = 100;
+    int N, S, arr[100], sum = 0;
     cin >> N >> S;
-    int arr[100];
     for(int i = 0; i < N; i++)
     {
         cin >> arr[i];
-        n_sum += arr[i];
+        sum += arr[i];
     }
+    int min = 10000, t_min, t_sum;
     for(int i = 0; i < N - 1; i++)
     {
         for(int j = i + 1; j < N; j++)
         {
-            tn_sum = n_sum - arr[i] - arr[j];
-            if(S - tn_sum >= 0)
+            t_sum = sum - arr[i] - arr[j];
+            if(S - t_sum >= 0)
             {
-                min = S - tn_sum;
+                t_min = S - t_sum;
             }
             else
             {
-                min = tn_sum - S;
+                t_min = t_sum - S;
             }
-            if(MIN >= min)
+            if(min > t_min)
             {
-                MIN = min;
+                min = t_min;
             }
         }
     }
-    cout << MIN << endl;
+    cout << min << endl;
     return 0;
 }
