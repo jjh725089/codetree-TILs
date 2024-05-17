1b372e935517fcd26370f08ee36854ef01d84b69
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-05-17 16:25:42 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240517/최고의 33위치/best-place-of-33.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, max = -1, sum = 0, cnt = 0;
+    cin >> n;
+    int arr[20][20];
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 0; i < n - 2; i++)
+    {
+        for(int j = 0; j < n - 2; j++)
+        {
+            cnt = 0;
+            while (cnt <= 3)
+            {
+            sum += arr[i+cnt][j] + arr[i+cnt][j+1] + arr[i+cnt][j+2];
+            cnt++;
+            if (i+cnt >= n)
+            break;
+            }
+            if(sum > max)
+            max = sum;
+        }
+    }
+    cout << max;
+    /*
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }    
+    */
+    return 0;
+}
