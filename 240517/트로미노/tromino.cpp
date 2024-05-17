92c7d07ed96086167ac9745ce217e51d1f29a8b1
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-05-17 22:22:12 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240517/트로미노/tromino.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, arr[200][200], max = 0, sum = 0;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    //3칸 한줄짜리
+    //col
+    for(int i = 0; i < n - 2; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            sum = arr[i][j] + arr[i + 1][j] + arr[i + 2][j];
+            if(sum > max)
+            max = sum;
+            sum = 0;
+        }
+    }
+    for(int j = 0; j < n - 2; j++)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 1];
+            if(sum > max)
+            max = sum;
+            sum = 0;
+        }
+    }
+    //한 칸을 기준으로 상(좌,우) 하(좌,우) 
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            //if(i - 1 < 0 || i + 1 == n || j - 1 < 0 || j + 1 == n)
+            //break;
+            if(j + 1 == n || i + 1 == n)
+            sum = 0;
+            else
+            {
+                sum = arr[i][j] + arr[i][j+1] + arr[i+1][j];
+                if(sum > max)
+                max = sum;
+            }
+            if(j + 1 == n || i - 1 < 0)
+            sum = 0;
+            else
+            {
+                sum = arr[i][j] + arr[i][j+1] + arr[i-1][j];
+                if(sum > max)
+                max = sum;
+            }
+            if(j - 1 < 0 || i + 1 == n)
+            sum = 0;
+            else
+            {
+                sum = arr[i][j] + arr[i][j-1] + arr[i+1][j];
+                if(sum > max)
+                max = sum;
+            }
+            if(j - 1 < 0 || i - 1 < 0)
+            sum = 0;
+            else
+            {
+                sum = arr[i][j] + arr[i][j-1] + arr[i-1][j];
+                if(sum > max)
+                max = sum;
+            }
+        }
+    }
+    cout << max << endl;
+    return 0;
+}
