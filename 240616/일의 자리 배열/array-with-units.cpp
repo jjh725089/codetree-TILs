40049efb9b52320d29fad37c9b17f62cd32bb466
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-06-16 02:28:49 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240616/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin >> a >> b;
+    int arr[10];
+    arr[0] = a;
+    arr[1] = b;
+    cout << arr[0] << " " << arr[1] << " ";
+    for(int i = 2; i < 10; i++)
+    {
+        arr[i] = arr[i - 2] + arr[i - 1];
+        cout << arr[i] % 10 << " ";
+    }
+    return 0;
+}
